d138f3a4995e97b8db074ed1aa54f48b112e8b19
Gergő Szabó <gary89@windowslive.com>
2020-12-30 19:41:19 +0100

    added filesize, directory detection and nice ui style

--- simple-serve.js
+++ simple-serve.js
@@ -6,6 +6,43 @@ const PORT = Number.parseInt(process.env.PORT || '9001');
 
 const dockerized = process.argv.length === 4 && process.argv[3] === 'dockerized';
 
+const KB = 1024, MB = KB * 1024, GB = MB * 1024;
+const MAP = '&lt;DIR&gt;';
+const page = `
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>SimpleServe</title>
+<style>
+	body {
+		display: flex;
+		flex-direction: column;
+		padding-bottom: 2em;
+	}
+	body > div {
+	    align-items: center;
+		display: flex;
+		justify-content: space-between;
+		padding-bottom: 0.5em;
+	}
+	body,
+	body>div:nth-child(odd) {
+	    background-color: #ddd;
+	}
+	body>div:nth-child(even) {
+	    background-color: #ccc;
+	}
+	a {
+		display: inline-block;
+		max-width: 80%;
+		padding-top: 0.5em;
+		word-wrap: break-word;
+	}
+</style>
+</head>
+<body>`;
+
 if (!dockerized && process.argv.length < 3) {
     console.error('Missing argument: directory');
     process.exit(-1);
@@ -16,6 +53,12 @@ if (!fs.existsSync(directory)) {
     console.error('Directory not exists', directory);
     process.exit(-1);
 }
+
+const toFriendlySize = size =>
+    size > GB ? `${(size / GB).toFixed(1)} GB` :
+        size > MB ? `${(size / MB).toFixed(1)} MB` :
+            size > KB ? `${(size / KB).toFixed(0)} KB` :
+                `${size} B&nbsp;`;
 
 const listener = (req, res) => {
     const filename = path.resolve(path.join(directory, req.url));
@@ -35,23 +78,37 @@ const listener = (req, res) => {
         const parentIsRoot = !!splitUrl.filter(Boolean).length;
         const parentDirectory = splitUrl.slice(undefined, splitUrl.length - 1);
         const mappedFiles = fs.readdirSync(filename)
-            .map(f => ({
-                name: f,
-                path: filename === directory ? `/${f}` : `${req.url}/${f}`
-            }));
+            .map(f => {
+                let fstat;
+                try {
+                    fstat = fs.statSync(path.join(filename, f));
+                } catch (e) {
+                    fstat = {isDirectory: () => false, size: -1};
+                }
+                return {
+                    name: f,
+                    path: filename === directory ? `/${f}` : `${req.url}/${f}`,
+                    size: fstat.isDirectory() ? MAP : toFriendlySize(fstat.size)
+                };
+            });
         const files = parentIsRoot ? [{
             name: '..',
-            path: parentDirectory.join('/')
+            path: parentDirectory.join('/'),
+            size: MAP
         }].concat(...mappedFiles) : mappedFiles;
-        const response = files.map(f => `<a href="//${host}${f.path}">${f.name}</a>`).join('<br />');
+        files.sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
+        const response = files.map(f =>
+            `<div><a href="//${host}${f.path}">${f.name}</a><span>${f.size}</span></div>`
+        ).join('\n');
         res.writeHead(200, {ContentType: 'text/html'});
-        res.end(response);
+        res.end(page + response + '</html>');
         return;
     }
 
     res.writeHead(200, {
         'Content-Type': 'application/octet-stream',
-        'Content-Disposition': filename.split('/').pop()
+        'Content-Disposition': filename.split('/').pop(),
+        'Content-Length': stat.size
     });
     fs.createReadStream(filename).pipe(res);
 };
