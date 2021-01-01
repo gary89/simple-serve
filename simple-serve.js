6afbc3435806a7104d6c18d47b66f0c74988eb57
Gergő Szabó <gary89@windowslive.com>
2021-01-01 16:34:06 +0100

    add option to wget a directory

--- simple-serve.js
+++ simple-serve.js
@@ -60,10 +60,96 @@ const toFriendlySize = size =>
             size > KB ? `${(size / KB).toFixed(0)} KB` :
                 `${size} B&nbsp;`;
 
-const listener = (req, res) => {
-    const filename = path.resolve(path.join(directory, req.url));
+const serveDirectory = (req, res, stat, filename) => {
     const host = req.headers['host'];
-    console.log(directory, req.url, filename, host);
+    const splitUrl = req.url.split('/');
+    const parentIsRoot = !!splitUrl.filter(Boolean).length;
+    const parentDirectory = splitUrl.slice(undefined, splitUrl.length - 1);
+    const mappedFiles = fs.readdirSync(filename)
+        .map(f => {
+            let fstat;
+            try {
+                fstat = fs.statSync(path.join(filename, f));
+            } catch (e) {
+                fstat = {isDirectory: () => false, size: -1};
+            }
+            return {
+                name: f,
+                path: filename === directory ? `/${f}` : `${req.url}/${f}`,
+                size: fstat.isDirectory() ? MAP : toFriendlySize(fstat.size)
+            };
+        });
+    const files = parentIsRoot ? [{
+        name: '..',
+        path: parentDirectory.join('/'),
+        size: MAP
+    }].concat(...mappedFiles) : mappedFiles;
+    files.sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
+    let response = files.map(f =>
+        `<div><a href="//${host}${f.path}">${f.name}</a><span>${f.size}</span></div>`
+    ).join('\n');
+    response += `<a href='//${host}/api/wget${req.url}'>WGET THIS FOLDER</a>`;
+    res.writeHead(200, {ContentType: 'text/html'});
+    res.end(page + response + '</html>');
+};
+
+const serveFile = (req, res, stat, filename) => {
+    console.log('serveFile', filename);
+    res.writeHead(200, {
+        'Content-Type': 'application/octet-stream',
+        'Content-Disposition': filename.split('/').pop(),
+        'Content-Length': stat.size
+    });
+    fs.createReadStream(filename).pipe(res);
+};
+
+const wgetDirectory = (req, res) => {
+    const url = req.url.replace('/api/wget', '');
+    const baseDirectory = url.replace(directory, '');
+    const filename = path.resolve(path.join(directory, url.replaceAll('%20', ' ')));
+    console.log(directory, req.url, filename);
+
+    const host = req.headers['host'];
+
+    const mapDirectory = dir => {
+        console.log('mapDirectory', dir)
+        const result = [];
+        const subDirectories = [];
+        fs.readdirSync(dir)
+            .forEach(f => {
+                try {
+                    const stat = fs.statSync(path.join(dir, f));
+                    if (stat.isDirectory()) {
+                        subDirectories.push(path.join(dir, f));
+                    } else {
+                        const d = dir.replace(directory, '').replaceAll(' ', '%20');
+                        result.push(`wget "http://${host}${d}/${f.replaceAll(' ', '%20')}" -O ${f.replaceAll(' ', '\\ ')}`);
+                    }
+                } catch (e) {
+                    console.error(`Error during stat ${f}`);
+                }
+            });
+
+        subDirectories
+            .map(subDir => {
+                const subDirResults = mapDirectory(subDir);
+                const purifiedSubDir = subDir.replace(dir, '').replace(directory, '').replace(baseDirectory, '').slice(1);//.replaceAll(' ', '%20');
+                result.push(`mkdir "${purifiedSubDir}" && cd ${purifiedSubDir.replaceAll(' ', '\\ ')}`);
+                result.push(...subDirResults);
+                result.push('cd ..');
+            });
+
+        return result;
+    };
+
+    const response = mapDirectory(filename).map(command => `<div>${command}</div>`).join('\n');
+    res.writeHead(200, {ContentType: 'text/html'});
+    res.end(page + response + '</html>');
+};
+
+const serveFileOrDirectory = (req, res) => {
+    const filename = path.resolve(path.join(directory, req.url.replaceAll('%20', ' ')));
+    console.log(directory, req.url, filename);
 
     if (!fs.existsSync(filename)) {
         res.writeHead(404, {ContentType: 'text/html'});
@@ -74,43 +160,18 @@ const listener = (req, res) => {
     const stat = fs.statSync(filename);
 
     if (stat.isDirectory()) {
-        const splitUrl = req.url.split('/');
-        const parentIsRoot = !!splitUrl.filter(Boolean).length;
-        const parentDirectory = splitUrl.slice(undefined, splitUrl.length - 1);
-        const mappedFiles = fs.readdirSync(filename)
-            .map(f => {
-                let fstat;
-                try {
-                    fstat = fs.statSync(path.join(filename, f));
-                } catch (e) {
-                    fstat = {isDirectory: () => false, size: -1};
-                }
-                return {
-                    name: f,
-                    path: filename === directory ? `/${f}` : `${req.url}/${f}`,
-                    size: fstat.isDirectory() ? MAP : toFriendlySize(fstat.size)
-                };
-            });
-        const files = parentIsRoot ? [{
-            name: '..',
-            path: parentDirectory.join('/'),
-            size: MAP
-        }].concat(...mappedFiles) : mappedFiles;
-        files.sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
-        const response = files.map(f =>
-            `<div><a href="//${host}${f.path}">${f.name}</a><span>${f.size}</span></div>`
-        ).join('\n');
-        res.writeHead(200, {ContentType: 'text/html'});
-        res.end(page + response + '</html>');
-        return;
+        serveDirectory(req, res, stat, filename);
+    } else {
+        serveFile(req, res, stat, filename);
     }
+};
 
-    res.writeHead(200, {
-        'Content-Type': 'application/octet-stream',
-        'Content-Disposition': filename.split('/').pop(),
-        'Content-Length': stat.size
-    });
-    fs.createReadStream(filename).pipe(res);
+const listener = (req, res) => {
+    if (req.url.startsWith('/api/wget')) {
+        wgetDirectory(req, res);
+    } else {
+        serveFileOrDirectory(req, res);
+    }
 };
 
 server.on('request', listener);
